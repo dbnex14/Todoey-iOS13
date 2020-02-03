ce67c3207daaddf3dbe41dbebf8ba3f7c9c5973d
dino buljubasic <dbnex14@gmail.com>
2020-02-03 15:35:32 -0800

    Modify storyboard to add new table view controller CategoryviewController and modify DataModel to add new entity Category and add 1:M relationship graph btw Category and Item.

---
+++ Todoey/Controllers/CategoryViewController.swift
@@ -0,0 +1,36 @@
+//
+//  CategoryViewController.swift
+//  Todoey
+//
+//  Created by Dino B on 2020-02-03.
+//  Copyright © 2020 App Brewery. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class CategoryViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // setup datasource
+    
+    // setup data manipulation methods
+    
+    // set up add button to add new categories
+
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+    }
+    
+    //MARK: TableView Datasource Methods
+    
+    
+    //MARK: TableView Delegate Methods
+    
+    
+    //MARK: Data Manipulation Methods
+    
+}
