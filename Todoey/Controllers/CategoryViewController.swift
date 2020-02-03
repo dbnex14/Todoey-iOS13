23acc34ca7ce8153e80ea5c2b115b4bdb3ae9536
dino buljubasic <dbnex14@gmail.com>
2020-02-03 15:59:59 -0800

    Add logic to create and persist todo categories.

--- Todoey/Controllers/CategoryViewController.swift
+++ Todoey/Controllers/CategoryViewController.swift
@@ -11,26 +11,70 @@ import CoreData
 
 class CategoryViewController: UITableViewController {
 
+    var categories = [Category]()
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        loadCategories()
     }
-    
-    // setup datasource
-    
-    // setup data manipulation methods
-    
-    // set up add button to add new categories
 
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
         
+        let alert = UIAlertController(title: "Add New Category", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add", style: .default) {(action) in
+            // what happens when user clicks Add button
+            let newCategory = Category(context: self.context)
+            newCategory.name = textField.text
+            self.categories.append(newCategory)
+            self.saveCategories()
+        }
+        alert.addAction(action)
+        alert.addTextField { (field) in
+            textField = field
+            textField.placeholder = "Add a new category"
+        }
+        present(alert, animated: true, completion: nil)
     }
     
     //MARK: TableView Datasource Methods
     
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return categories.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // create reusable cell and add it to the table at indes path
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
+        cell.textLabel?.text = categories[indexPath.row].name
+        // return cell so it is rendered on the screen
+        return cell
+    }
     
     //MARK: TableView Delegate Methods
     
     
     //MARK: Data Manipulation Methods
+    func loadCategories() {
+        let request: NSFetchRequest<Category> = Category.fetchRequest()
+        do {
+            categories = try context.fetch(request)
+        } catch {
+            print("Error loading categories \(error)")
+        }
+        
+        tableView.reloadData()
+    }
     
+    func saveCategories() {
+        do {
+            try context.save()
+        } catch {
+            print("Error saving category \(error)")
+        }
+        
+        tableView.reloadData()
+    }
 }
